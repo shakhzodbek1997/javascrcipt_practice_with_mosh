53f3bf6279f508927409995912ab07c79f341fdf
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-04-27 08:10:08 +0500

    js and html created

---
+++ index.js
@@ -0,0 +1,17 @@
+let baseSalary = 30_000;
+let overtime = 10;
+let rate = 20;
+
+function getWage(baseSalary, overtime, rate) {
+    return baseSalary + (overtime * rate);
+}
+
+let employee = {
+    baseSalary: 30_000,
+    overtime: 10,
+    rate: 20,
+    getWage: function () {
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+employee.getWage();
