f55ef2f39492f6a47e3197a4ce0e8fae84ba3374
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-05-01 18:13:36 +0500

    example 1

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-}
+let x = {value: 10};
+let y = x;
 
-const Circle1 = new Function('radius', `
-        this.radius = radius;
-        this.draw = function(){
-        console.log('draw');
-    }
-`);
-
-const circle = new Circle1(1);
-
-const another = new Circle(1);
+x.value  = 20;
