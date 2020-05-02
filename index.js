4399063c01528d2e50e9456754f7c4a293c5aee7
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-05-02 11:59:02 +0500

    enumerating properties

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-let x = {value: 10};
-let y = x;
+// let x = {value: 10};
+// let y = x;
+//
+// x.value  = 20;
 
-x.value  = 20;
+// let obj = {value: 10};
+// function increase(obj ) {
+//     obj.value++;
+// }
+//
+// increase(obj);
+// console.log(obj.value);
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(10);
+
+for (let key in circle){
+    if (typeof circle[key] !=='function')
+    console.log(key, circle[key]);
+}
+
+const  keys = Object.keys(circle);
+console.log(keys);
+
+if ('radius' in circle){
+    console.log('Circle has a radius');
+}
