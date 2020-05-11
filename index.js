771020bd9fc469c694f2f2a1e96382b9ce8478ed
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-05-11 18:42:48 +0500

    getter and setter

--- index.js
+++ index.js
@@ -36,21 +36,30 @@
 function Circle(radius) {
     this.radius = radius;
 
-    this.defaultLocation = {x: 0, y: 0};
+    let defaultLocation = {x: 0, y: 0};
 
-    this.computeOptimumLocation = function (factor) {
-        //...
+    this.getDefaultLocation = function(){
+        return defaultLocation;
     };
 
-    this.draw = function(){
-        this.computeOptimumLocation(0.1);
-
+    this.draw = function () {
         console.log('draw');
     };
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function () {
+            return defaultLocation;
+        },
+        set: function (value) {
+            if (!value.x || !value.y)
+                throw new Error('Invalid location.')
+            defaultLocation = value;
+        }
+    });
 }
 
 const circle = new Circle(10);
-circle.computeOptimumLocation(0.1);
+circle.defaultLocation = 1;
 circle.draw();
 
 
