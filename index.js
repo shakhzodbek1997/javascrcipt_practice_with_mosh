93fefca9d95acae8ef00680f285cafa375e2d8ad
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-05-11 18:24:52 +0500

    abstraction

--- index.js
+++ index.js
@@ -11,23 +11,72 @@
 // increase(obj);
 // console.log(obj.value);
 
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// }
+//
+// const circle = new Circle(10);
+//
+// for (let key in circle){
+//     if (typeof circle[key] !=='function')
+//     console.log(key, circle[key]);
+// }
+//
+// const  keys = Object.keys(circle);
+// console.log(keys);
+//
+// if ('radius' in circle){
+//     console.log('Circle has a radius');
+// }
+
+/*----------------ABSTRACTION---------------*/
 function Circle(radius) {
     this.radius = radius;
-    this.draw = function () {
+
+    this.defaultLocation = {x: 0, y: 0};
+
+    this.computeOptimumLocation = function (factor) {
+        //...
+    };
+
+    this.draw = function(){
+        this.computeOptimumLocation(0.1);
+
         console.log('draw');
-    }
+    };
 }
 
 const circle = new Circle(10);
+circle.computeOptimumLocation(0.1);
+circle.draw();
 
-for (let key in circle){
-    if (typeof circle[key] !=='function')
-    console.log(key, circle[key]);
-}
 
-const  keys = Object.keys(circle);
-console.log(keys);
 
-if ('radius' in circle){
-    console.log('Circle has a radius');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
