9e372db3d4d4b88130bea5448f8e95bd12c1122e
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-05-12 15:27:24 +0500

    MY STOPWATCH

--- index.js
+++ index.js
@@ -33,44 +33,81 @@
 // }
 
 /*----------------ABSTRACTION---------------*/
-function Circle(radius) {
-    this.radius = radius;
+// function Circle(radius) {
+//     this.radius = radius;
+//
+//     let defaultLocation = {x: 0, y: 0};
+//
+//     this.getDefaultLocation = function(){
+//         return defaultLocation;
+//     };
+//
+//     this.draw = function () {
+//         console.log('draw');
+//     };
+//
+//     Object.defineProperty(this, 'defaultLocation', {
+//         get: function () {
+//             return defaultLocation;
+//         },
+//         set: function (value) {
+//             if (!value.x || !value.y)
+//                 throw new Error('Invalid location.');
+//             defaultLocation = value;
+//         }
+//     });
+// }
+//
+// const circle = new Circle(10);
+// circle.defaultLocation = 1;
+// circle.draw();
 
-    let defaultLocation = {x: 0, y: 0};
 
-    this.getDefaultLocation = function(){
-        return defaultLocation;
+
+/*
+//-----------Task - Stop Watch-------------------
+
+function StopWatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function(){
+        if (running)
+            throw  new Error('Stopwatch has already started!');
+
+        running = true;
+
+        startTime = new Date();
     };
 
-    this.draw = function () {
-        console.log('draw');
+    this.stop = function () {
+        if (!running)
+           throw new Error('Stopwatch is not started!');
+
+        running = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime())/1000;
+        duration += seconds;
     };
 
-    Object.defineProperty(this, 'defaultLocation', {
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+    Object.defineProperty(this, 'duration', {
         get: function () {
-            return defaultLocation;
-        },
-        set: function (value) {
-            if (!value.x || !value.y)
-                throw new Error('Invalid location.')
-            defaultLocation = value;
+            return duration;
         }
     });
 }
-
-const circle = new Circle(10);
-circle.defaultLocation = 1;
-circle.draw();
+*/
 
 
-
-
-
-
-
-
-
-
+// ------------------my Stopwatch JS-----------------
 
 
 
